d7781816cce00c7a97d0e11eec7c8abe09c02d86
jerobar <jerobar@pm.me>
2023-11-05 16:21:54 +0800

    week 4 doneish but buggy

---
+++ week-4/Prover.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+
+contract Prover {
+    uint256 constant CURVE_ORDER =
+        0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001;
+
+    struct G1Point {
+        uint256 x;
+        uint256 y;
+    }
+
+    struct G2Point {
+        uint256[2] x;
+        uint256[2] y;
+    }
+
+    // G1
+    G1Point G1 = G1Point(1, 2);
+    // A₁
+    G1Point Alpha_1 =
+        G1Point(
+            5138697240077803445514669414784254799933862402946278134326199877546184124353,
+            12587011617949543324467535889916856826666519601316494966427400843934921824601
+        );
+    // B₂
+    G2Point Beta_2 =
+        G2Point(
+            [
+                15380484938200814403881087981412294620100873432818036673589291147845024225571,
+                12602109964935088016887987406157853419623515645637587704176662081914411301318
+            ],
+            [
+                18666296425544234789562310040634234422589269469188454772496605606699412541097,
+                5196377417757280559932251044816639832790442126146965060826533782304458949952
+            ]
+        );
+    // γ₂
+    G2Point Gamma_2 =
+        G2Point(
+            [
+                16533935655882508933200494473493224172137214331738653888303739254278392816139,
+                18880978986792929021469791843695392603128946481846666271410481427998908587420
+            ],
+            [
+                13141448804549710154358653194644535861993981907731549996410137978037004389569,
+                9762548523825741675364125428211388275252455058307432435140067326598456040331
+            ]
+        );
+    // δ₂
+    G2Point Detla_2 =
+        G2Point(
+            [
+                10162338589393961187840690440736730403643501083670401659044175493788672801975,
+                15778097377978213295342357580990122748603225141808559681543747573981325835864
+            ],
+            [
+                10296903209504479081730603432149203031278495964641591619128682912272764993749,
+                18440560316284345279041150568082080240188217139841085848344914718164626752030
+            ]
+        );
+
+    function addPoints(
+        G1Point memory A,
+        G1Point memory B
+    ) public view returns (G1Point memory) {
+        (bool success, bytes memory result) = address(6).staticcall(
+            abi.encode(A.x, A.y, B.x, B.y)
+        );
+        require(success, "Prover: point addition failed.");
+
+        (uint256 x, uint256 y) = abi.decode(result, (uint256, uint256));
+        return G1Point(x, y);
+    }
+
+    function scalarMultiplyPoint(
+        G1Point memory P,
+        uint256 s
+    ) public view returns (G1Point memory) {
+        (bool success, bytes memory result) = address(7).staticcall(
+            abi.encode(P.x, P.y, s)
+        );
+        require(success, "Prover: point scalar multiplication failed.");
+
+        (uint256 x, uint256 y) = abi.decode(result, (uint256, uint256));
+        return G1Point(x, y);
+    }
+
+    function negatePoint(
+        G1Point memory P
+    ) public pure returns (G1Point memory) {
+        if (P.x == 0 && P.y == 0) {
+            return G1Point(0, 0);
+        } else {
+            // Additive inverse of P (reflected over x-axis)
+            return G1Point(P.x, CURVE_ORDER - (P.y % CURVE_ORDER));
+        }
+    }
+
+    function verify(
+        G1Point calldata A1,
+        G2Point calldata B2,
+        G1Point calldata C1,
+        uint256 x1,
+        uint256 x2,
+        uint256 x3
+    ) public view returns (bool verified) {
+        // Compute X1 = x1G1 + x2G1 + x3G1
+        G1Point memory X1 = addPoints(
+            addPoints(scalarMultiplyPoint(G1, x1), scalarMultiplyPoint(G1, x2)),
+            scalarMultiplyPoint(G1, x3)
+        );
+
+        // Verify the equality 0 = -A₁B₂ + α₁β₂ + X₁γ₂ + C₁δ₂
+        G1Point memory neg_A1 = negatePoint(A1);
+        (bool success, bytes memory result) = address(8).staticcall(
+            abi.encode(
+                // -A₁B₂
+                neg_A1.x,
+                neg_A1.y,
+                B2.x[0],
+                B2.x[1],
+                B2.y[0],
+                B2.y[1],
+                // α₁β₂
+                Alpha_1.x,
+                Alpha_1.y,
+                Beta_2.x[0],
+                Beta_2.x[1],
+                Beta_2.y[0],
+                Beta_2.y[1],
+                // X₁γ₂
+                X1.x,
+                X1.y,
+                Gamma_2.x[0],
+                Gamma_2.x[1],
+                Gamma_2.y[0],
+                Gamma_2.y[1],
+                // C₁δ₂
+                C1.x,
+                C1.y,
+                Detla_2.x[0],
+                Detla_2.x[1],
+                Detla_2.y[0],
+                Detla_2.y[1]
+            )
+        );
+
+        verified = abi.decode(result, (bool));
+    }
+}
---
+++ week-4/scratch.py
@@ -0,0 +1,136 @@
+from py_ecc.bn128 import multiply, G1, G2
+
+# Scalars
+x1 = 13
+x2 = 18
+x3 = 29
+
+a_1 = 69 # will be treated as negative
+b_2 = 420
+
+alpha_1 = 22
+beta_2 = 44
+
+x_1 = x1 + x2 + x3
+gamma_2 = 89
+
+c_1 = 109
+delta_2 = 208
+
+# Sanity check
+print("Scalars sum to 0:", -a_1*b_2 + alpha_1*beta_2 + x_1*gamma_2 + c_1*delta_2 == 0)
+
+# Elliptic curve points
+A1 = multiply(G1, a_1) # negated in Solidity contract
+B2 = multiply(G2, b_2)
+C1 = multiply(G1, c_1)
+print("A1:\n", A1)
+print("B2:\n", B2)
+print("C1:\n", C1)
+
+Alpha_1 = multiply(G1, alpha_1)
+Beta_2 = multiply(G2, beta_2)
+Gamma_2 = multiply(G2, gamma_2)
+Delta_2 = multiply(G2, delta_2)
+print("Alpha_1:\n", Alpha_1)
+print("Beta_2:\n", Beta_2)
+print("Gamma_2:\n", Gamma_2)
+print("Delta_2:\n", Delta_2)
+
+
+
+
+# from py_ecc.bn128 import G1, G2, neg, pairing, add, multiply, eq
+
+# # G1 and G2 are generator points for their respective groups
+# print(G1) # (x, y)
+# print(G2) # ((x, y), (x, y))
+# # While G2 may seem strange, it behaves like other cyclic groups
+
+# # print(G1 + G1 + G1 == G1*3)
+# # # True
+# # # The above is the same as this:
+# # eq(add(add(G1, G1), G1), multiply(G1, 3))
+
+# A = multiply(G2, 5) # Lib requires G2 as first arg
+# B = multiply(G1, 6)
+# C = multiply(G2, 5 * 6)
+# # or C = multiply(G1, 5*6)
+# pairing(A, B) == pairing(C, G1) # True
+
+# # paring returns a member of G12
+# # e: G1 × G2 → G12
+
+# # 0 = -A1B2 + a1bet2 + X1gam2 + C1om2
+
+
+# # If: ab + cd = 0
+# # Then: A1B2 + C1D2 = 012 in G12 space
+
+# x1 = 0
+# x2 = 0 
+# x3 = 0
+
+# a = 6
+# b = 4
+
+# c = 2
+# d = 4
+
+# e = x1 + x2 + x3
+# f = 8
+
+# g = 4
+# h = 2
+
+# # -ab + cd + ef + gh = 0
+
+# # neg(multiply(G1, a))
+# a_b = pairing(neg(multiply(G1, a)), multiply(G2, b))
+# c_d = pairing(multiply(G1, c), multiply(G1, d))
+# e_f = pairing(multiply(G1, e), multiply(G1, f))
+# g_h = pairing(multiply(G1, g), multiply(G1, h))
+
+
+
+
+# """
+# Blinear pairings allow us to take a, b, and c, encrypt them to 
+# become E(a), E(b), and E(c), then send the three encrypted values
+# to a verifier who can verify:
+# E(a) * E(b) = E(c)
+
+# p * q = r
+# F(P, Q) = F(R)
+# p * q = r * 1
+# F(P,Q) = F(R,G) # Generator point G is "1" in this context
+
+# f(aG, bG) = f(abG, G) = f(G, abG)
+
+# A bilinear pairing is a mapping:
+# e(aG, bG) = e(abG, G) = e(G, abG)
+# e: G x G -> Gt (G target group)
+
+# Symmetric/asymmetric
+
+# Bilinear pairings are easier with different groups:
+# e(G, G') -> G"
+# e(aG, bG') = e(abG, G') = e(G, abG') # still holds
+# # G" is the codomain (outside space) of e(G, G')
+
+# Ethereum's bilinear pairing of choice uses elliptic curves with
+# field extensions. We think of ec points as (x, y) but with field 
+# extensions they consist of several (x, y) pairs. It is analagous to 
+# how complex numbers extend real numbers. EC points with more than 2 
+# dimensions.
+
+# Bilinearity is a property that is hard to come by. the chances the 
+# relationship holds for three random groups is slim. Using groups of 
+# different dimensions, they become easier to construct. 
+
+# G1, G2, and G12 (numbers correspond to number of dimensions)
+
+# The py_ecc library is maintain by the Ethereum foundation. It 
+# powers the precompile address at 0x8 in the pyEVM implementation. 
+# The precompile works on points in G1, G2, and implicitly G12.
+# """
