02e5fc55ec3cd1624e5e627a6b7f289abd5ed251
jerobar <jerobar@pm.me>
2023-11-07 16:59:08 +0800

    really should work

--- week-4/contracts/Prover.sol
+++ week-4/contracts/Prover.sol
@@ -203,17 +203,24 @@ contract Prover {
         console.log("Delta2.y2", input[22]);
         console.log("Delta2.y1", input[23]);
 
-        bool success = false;
-
         assembly {
-            success := staticcall(gas(), 8, input, mul(24, 0x20), input, 0x20)
-            if success {
-                return(input, 0x20)
-            }
+            let success := staticcall(gas(), 8, input, mul(24, 0x20), input, 0x20)
+            if success { return(input, 0x20) }
         }
 
-        console.log("success:", success);
-
         return false;
+
+        // bool success = false;
+
+        // assembly {
+        //     success := staticcall(gas(), 8, input, mul(24, 0x20), input, 0x20)
+        //     if success {
+        //         return(input, 0x20)
+        //     }
+        // }
+
+        // console.log("success:", success);
+
+        // return false;
     }
 }
